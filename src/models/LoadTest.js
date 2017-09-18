be1172c87088318c338fd1898776831bf1863b6a
Matthias Jäggli <matthias.jaeggli@gmail.com>
2017-09-18 12:58:13 +0200

    it works

---
+++ src/models/LoadTest.js
@@ -0,0 +1,105 @@
+const fetch = require('node-fetch')
+const now = require('performance-now')
+
+class LoadTest {
+  constructor ({
+      wait = 500,
+      instances = 1,
+      urls = ['/'],
+      values = [],
+      onSuccess = () => true,
+      onFail = () => true,
+      onRequest = () => true,
+      verification = () => true
+    }) {
+    this.wait = wait
+    this.instances = instances
+    this.urls = urls
+    this.values = values
+    this.onSuccess = onSuccess
+    this.onFail = onFail
+    this.onRequest = onRequest
+    this.verification = verification
+    this.timers = []
+    this.running = false
+    this.stop = this.stop.bind(this)
+    this.start = this.start.bind(this)
+    this.request = this.request.bind(this)
+  }
+  getRandomValue (arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+  }
+  getUrl () {
+    let url = this.getRandomValue(this.urls)
+    let lang
+    Object.keys(this.values).forEach(key => {
+      let value = this.getRandomValue(this.values[key])
+      url = url.replace(
+        `{${key}}`,
+        value
+      )
+      if (key === 'lang') { lang = value }
+    })
+    return {
+      url,
+      lang
+    }
+  }
+  async request () {
+    this.stats.sent++
+    const time = now()
+    const { url, lang } = this.getUrl()
+    this.onRequest(url)
+    try {
+      const response = await fetch(url)
+      this.stats.received++
+      const delta = now() - time
+      if (response.status >= 200 && response.status < 300) {
+        const text = await response.text()
+        if (this.verification(text, lang)) {
+          this.onSuccess(response, delta, text)
+          this.stats.successDurations.push(delta)
+          return
+        }
+      }
+      this.stats.failedDurations.push(delta)
+      this.onFail(response, delta)
+    } catch (e) {}
+  }
+  start () {
+    if (this.running) { return }
+    this.running = true
+    this.stats = {
+      start: now(),
+      duration: null,
+      sent: 0,
+      received: 0,
+      successDurations: [],
+      failedDurations: []
+    }
+    for (let i = 0; i < this.instances; i++) {
+      const delay = Math.round(i * this.wait / this.instances)
+      setTimeout(() => {
+        this.timers.push(setInterval(() => {
+          this.request()
+        }, this.wait))
+        this.request()
+      }, delay)
+    }
+  }
+  stop () {
+    if (!this.running) { return }
+    this.running = false
+    this.timers.forEach(timer => {
+      clearTimeout(timer)
+    })
+    this.timers = []
+
+    // Fixme, wait until all requests responded
+    this.stats.duration = now() - this.stats.start
+    setTimeout(() => {
+    }, 1000)
+  }
+}
+
+module.exports = LoadTest
