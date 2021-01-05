cdd8fcc4d5ea4dcc04094052afa0daf7364f8d33
Matthias Jäggli <matthias.jaeggli@gmail.com>
2021-01-06 00:42:27 +0100

    Handle exit codes

--- src/runPorcelain.js
+++ src/runPorcelain.js
@@ -22,6 +22,34 @@ const runPorcelain = (config) => {
   console.log(JSON.stringify(bulk, null, 2));
   console.log("-----------------------");
 
+  // listen for exit signals
+  const exit = (cause) => {
+    loadTest.stop();
+    console.log(cause);
+    process.exit();
+  };
+  process.on("SIGTERM", () => exit("Received SIGTERM"));
+
+  // catch esc
+  const { stdin } = process;
+  stdin.setRawMode(true);
+  stdin.resume();
+  stdin.setEncoding("utf8");
+  stdin.on("data", (key) => {
+    const exitKeys = [
+      "\u0003", // ctrl-c
+      "\u001b", // esc
+      "\u0071", // q
+    ];
+
+    if (exitKeys.includes(key)) {
+      return exit("Aborted by user");
+    }
+
+    // write the key to stdout all normal like
+    process.stdout.write(key);
+  });
+
   // start run
   loadTest.start();
 };
